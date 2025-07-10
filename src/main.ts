3e2fe7dec11de7aa8352f138ddfbc5ffbea7e0dc
HA1602 <harinandanarun1602@gmail.com>
2025-07-10 15:48:04 +0530

    second comming

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+
+  app.enableCors({
+    origin: 'http://mytodolist-hari1.s3-website.ap-south-1.amazonaws.com/', // 👈 your frontend domain
+    methods: 'GET,POST,PUT,DELETE',
+  });
   await app.listen(3000);
 }
 bootstrap();
